041394a6135339eed98b86a3b7f18d8719f95a69
pmosbach <peter.c.mosbacher@nasa.gov>
2018-09-09 00:30:23 -0500

    Setup eslint and add empty docs folder test

--- repostructure.test.js
+++ repostructure.test.js
@@ -1,5 +1,5 @@
 "use strict";
-//--------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------
 // Dependencies
 
 // NPM-installed dependencies
@@ -8,53 +8,57 @@ const commitCount = require("git-commit-count");
 
 // Built-in Node dependencies
 const fs = require("fs");
-const path = require("path");
 
-//--------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------
 // Constants
 
-
-//--------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------
 // Tests
 
-// TODO resolve the path of the repo to be tested
+describe("The Repo - README and dotfiles", () => {
+  test("should contain a README", () => {
+    // TODO add the case insensitive option to make this less ugly
+    expect(
+      glob.sync("[Rr][Ee][Aa][Dd][Mm][Ee]?(.md|.txt|.rst)")
+    ).not.toHaveLength(0);
+  });
 
-describe('The Repo - README and dotfiles', () => {
-    test('should contain a README', () => {
-        // TODO add the case insensitive option to make this less ugly
-        expect(glob.sync("[Rr][Ee][Aa][Dd][Mm][Ee]?(\.md|\.txt|\.rst)")).not.toHaveLength(0);
-    })
+  test("should contain a .gitignore file", () => {
+    expect(fs.existsSync(".gitignore")).toBeTruthy();
+  });
 
-    test('should contain a .gitignore file', () => {
-        expect(fs.existsSync('.gitignore')).toBeTruthy();
-    })
+  test("should contain a GitLab CI file (.gitlab-ci.yml)", () => {
+    expect(fs.existsSync(".gitlab-ci.yml")).toBeTruthy();
+  });
+});
 
-    test('should contain a GitLab CI file (.gitlab-ci.yml)', () => {
-        expect(fs.existsSync('.gitlab-ci.yml')).toBeTruthy();
-    })
-})
+describe("The Repo - Directory Structure", () => {
+  test("should have a documentation folder", () => {
+    expect(glob.sync("@(doc|docs|Documents|Documentation)/")).not.toHaveLength(
+      0
+    );
+  });
 
-describe('The Repo - Directory Structure', () => {
-    test('should have a documentation folder', () => {
-        expect(glob.sync("@(doc|docs|Documents|Documentation)/")).not.toHaveLength(0);
-    })
+  test("should have a documentation folder that is not empty", () => {
+    expect(glob.sync("@(doc|docs|Documents|Documentation)/*")).not.toHaveLength(
+      0
+    );
+  });
 
-    /*test('should have a documentation folder that is not empty', () => {
-        
-    })*/
+  test("should not have a releases folder", () => {
+    expect(glob.sync("[Rr]elease?(s)/")).toHaveLength(0);
+  });
+});
 
-    test('should not have a releases folder', () => {
-        expect(glob.sync("[Rr]elease?(s)/")).toHaveLength(0);
-    })
-})
+describe("The Repo - Git Basics", () => {
+  test("should have more than one commit in the git history", () => {
+    expect(
+      commitCount(process.env.CI_PROJECT_DIR || process.cwd())
+    ).toBeGreaterThan(1);
+  });
+});
 
-describe('The Repo - Git Basics', () => {
-    test('should have more than one commit in the git history', () => {
-        expect(commitCount(process.env.CI_PROJECT_DIR || process.cwd())).toBeGreaterThan(1);
-    })
-})
-
-//console.log("CWD: " + process.cwd());
-//console.log("__dirname: " + __dirname);
-//console.log("PWD: " + process.env.PWD);
-//console.log("Commits: " + commitCount(process.env.CI_PROJECT_DIR || process.cwd()));
+// console.log("CWD: " + process.cwd());
+// console.log("__dirname: " + __dirname);
+// console.log("PWD: " + process.env.PWD);
+// console.log("Commits: " + commitCount(process.env.CI_PROJECT_DIR || process.cwd()));
